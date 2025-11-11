656953d47e49fc1bf318188877c0b542e25ef71d
Rithik <rithik@techies.it>
2025-11-11 11:40:37 +0530

    update package.json and vercel.json files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Virtual entry point for the app
 import {storefrontRedirect} from '@shopify/hydrogen';
 import {createRequestHandler} from '@shopify/remix-oxygen';
-import {createAppLoadContext} from '~/lib/context';
+import {createAppLoadContext} from './app/lib/context.js';
 
 export default {
   async fetch(request, env, executionContext) {
